a0c64b448e5c0e2a27daa0f0f33bfa1ee4b48c19
dmtr636 <dmtr636@gmail.com>
2022-09-03 14:53:17 +0300

    Version 1.0

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,2 +1,2 @@
-export const SERVER_HOST = "http://localhost:8000"
-//export const SERVER_HOST = "https://3950-185-135-149-4.eu.ngrok.io"
+export const SERVER_HOST = "https://kodim.space"
+//export const SERVER_HOST = "http://localhost:8000"
